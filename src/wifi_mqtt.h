459620abda11e5e9542eebe9aa3d29a6e42f73c4
bartje <witje1@gmail.com>
2023-08-16 18:55:54 +0200

    Update wifi_mqtt.h

--- src/wifi_mqtt.h
+++ src/wifi_mqtt.h
@@ -15,7 +15,7 @@ class wifi_mqtt
 {
   public:
     wifi_mqtt(String wifiSsid, String wifiPassword);
-    wifi_mqtt(char wifiSsid, char wifiPassword, char mqttServer);
+    wifi_mqtt(String wifiSsid, String wifiPassword, char mqttServer);
     
     void begin();
     bool connect_wifi();
